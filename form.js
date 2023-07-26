e9da3d58e461db1c2baa3f7bdf2b6b9cca46ee04
Felicitas Kroder <Felicitas.Kroder@gmail.com>
2023-07-26 12:46:48 +0200

    creating all elements for new Card

--- form.js
+++ form.js
@@ -20,7 +20,9 @@ form.addEventListener("submit", (event) => {
   const cardButton = document.createElement("button");
   const cardAnswer = document.createElement("p");
   const cardTagList2 = document.createElement("ul");
-  const cardTagListItem = document.createElement("li");
+  const cardTagListItem1 = document.createElement("li");
+  const cardTagListItem2 = document.createElement("li");
+  const cardTagListItem3 = document.createElement("li");
 
   // ADD CSS
   cardTaglist1.classList.add("card-list");
@@ -29,14 +31,20 @@ form.addEventListener("submit", (event) => {
   cardQuestion.classList.add("card__question");
   cardButton.classList.add("card__button-answer");
   cardAnswer.classList.add("card__answer");
+  cardTagList2.classList.add("card__tag-list");
+  cardTagListItem1.classList.add("card__tag-list-item");
+  cardTagListItem2.classList.add("card__tag-list-item");
+  cardTagListItem3.classList.add("card__tag-list-item");
 
   // APPEND CARD
   cardContainer.append(cardTaglist1);
-  cardTaglist1.append(
-    newCard,
+  cardTaglist1.append(newCard);
+  newCard.append(
     cardArticle,
     cardQuestion,
     cardButton,
-    cardAnswer
+    cardAnswer,
+    cardTagList2
   );
+  cardTagList2.append(cardTagListItem1, cardTagListItem2, cardTagListItem3);
 });
